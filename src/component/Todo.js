5bd7051270b96d4d2887154252478c717d9a8d1d
Abena Apaw <akapaw@wustl.edu>
2020-04-15 19:54:49 -0400

    Corrected error with todo.js

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,10 +1,5 @@
 import React from 'react'
-/* import { connect } from "react-redux";
-import cx from "classnames";
-import { toggleTodo } from "../actions"; */
 import PropTypes from 'prop-types'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import FlipMove from 'react-flip-move';
 
 const Todo = ({ onClick, completed, text }) => (
     <li
@@ -16,39 +11,8 @@ const Todo = ({ onClick, completed, text }) => (
         {text}
         
     </li>
-     
-   
+        
 )
-function Todo(props) {
-
-    const todos = props.todos;
-
-    const Todo = ({ onClick, completed, text }) => (
-return
-        <div className="list" key={todo.key}>
-            <p>
-                <input type="text" id={todo.key} value={todo.text} onChange={(e) => {
-                    props.setUpdate(e.target.value, todo.key)
-                }} />
-
-                <span>
-                    <FontAwesomeIcon className="faicons" onClick={() => {
-                        props.deleteItem(item.key)
-                    }} icon="trash" />
-                </span>
-            </p>
-        </div>
-    })
-
-    return <div>
-
-        <FlipMove duration={300} easing="ease-in-out">
-            {Todo}
-        </FlipMove>
-    </div>;
-}
-
-// export default ListItems;
 
 Todo.propTypes = {
     onClick: PropTypes.func.isRequired,
