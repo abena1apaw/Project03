b47d81f094124d31363c8056e699eaf887a1b7b0
Abena Apaw <akapaw@wustl.edu>
2020-04-26 10:00:13 -0400

    Changed styles.

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   const headerStyle = {
-    backgroundColor: "#678c89",
+    backgroundColor: "#800080",
     color: "#fff",
     padding: "10px 15px",
   };
