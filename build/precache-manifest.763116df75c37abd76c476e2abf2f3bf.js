01105194c59e213c327d3eb210cd645ab8380309
abena1apaw <57848351+abena1apaw@users.noreply.github.com>
2020-04-26 21:24:35 -0500

    changed app

---
+++ build/precache-manifest.763116df75c37abd76c476e2abf2f3bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21aa6d35302c03503352096a845e502f",
+    "url": "/Project03/index.html"
+  },
+  {
+    "revision": "75937ff914c2d6b2d6d4",
+    "url": "/Project03/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "550dd1e55b3bba15a1ad",
+    "url": "/Project03/static/css/main.a771c4dd.chunk.css"
+  },
+  {
+    "revision": "75937ff914c2d6b2d6d4",
+    "url": "/Project03/static/js/2.2cd97164.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Project03/static/js/2.2cd97164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550dd1e55b3bba15a1ad",
+    "url": "/Project03/static/js/main.21008f76.chunk.js"
+  },
+  {
+    "revision": "afaf8e749a24cce67178",
+    "url": "/Project03/static/js/runtime-main.b240eab9.js"
+  }
+]);
